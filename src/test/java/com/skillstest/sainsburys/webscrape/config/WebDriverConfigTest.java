174bb00a498d88b7dcb0943dbd2eaadc908b98ac
Colin Schofield <colin_sch@yahoo.com>
2017-06-04 22:01:53 +0100

    Changed back the WebDriver

--- src/test/java/com/skillstest/sainsburys/webscrape/config/WebDriverConfigTest.java
+++ src/test/java/com/skillstest/sainsburys/webscrape/config/WebDriverConfigTest.java
@@ -2,6 +2,7 @@ package com.skillstest.sainsburys.webscrape.config;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import static org.junit.Assert.assertTrue;
@@ -12,7 +13,7 @@ public class WebDriverConfigTest {
     @Test
     public void testWebDriver() throws Exception {
 
-        WebDriver driver = new SafariDriver();
+        WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://google.com");
         Thread.sleep(2000);
         assertTrue("Check that we can load a standard web page, if not, make sure that there is a Selenium Web Driver is loaded!",
